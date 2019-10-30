60a07cb314145a876408bbde188d7dfb93c9a40c
Maria Luiza <malurlmachado@gmail.com>
2019-10-30 13:32:50 -0300

    Create Table da classe/tabela Centro de Custo criada

--- src/DAO/CentroCustoDAO.java
+++ src/DAO/CentroCustoDAO.java
@@ -5,21 +5,29 @@ import model.CentroCusto;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class CentroCustoDAO {
 
     Connection conn = null;
+    private final static String CREATE_TABLE = "create table Centro_Custo (\n" +
+            "nr_sequencia integer,\n" +
+            "ds_centro_custo varchar(25)\n" +
+            ");";
+    public void createTable() throws SQLException {
+        PreparedStatement pst = Conexao.conn.prepareStatement(CREATE_TABLE);
+        pst.executeQuery();
 
-    public void salvar (CentroCusto centroCusto) throws Exception{
+    }
 
+
+    public void salvar (CentroCusto centroCusto) throws Exception {
         String sql = "INSERT INTO(NR_SEQUENCIA, DS_CENTRO_CUSTO) CENTRO_CUSTO VALUES (?,?)";
         PreparedStatement pst = Conexao.conn.prepareStatement(sql);
         pst.setLong(1, centroCusto.getNrSequencia());
         pst.setString(2, centroCusto.getDsCentroCusto());
         pst.executeUpdate();
-
-
     }
 
 }
